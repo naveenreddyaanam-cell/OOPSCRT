b0bb437d4bdc616807099cd05586c57075e1d10c
naveenreddyaanam-cell <naveenreddyaanam@gmail.com>
2025-10-25 16:13:14 +0530

    Add files via upload

---
+++ super funtion.py
@@ -0,0 +1,11 @@
+class Parent:
+   def __init__(self, name):
+       self.name = name
+class Child(Parent):
+   def __init__(self, name, age):
+       super().__init__(name)
+       self.age = age
+child = Child("Alice", 10)
+print(child.name)
+print(child.age)
+                                         
