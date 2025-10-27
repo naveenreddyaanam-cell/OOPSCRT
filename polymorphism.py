8c4ab144188d0d8dcc00769c9f3dd11ddf58d93e
naveenreddyaanam-cell <naveenreddyaanam@gmail.com>
2025-10-27 14:27:24 +0530

    Add files via upload

---
+++ polymorphism.py
@@ -0,0 +1,12 @@
+#polymorphism in Built-in function len()
+
+students = ['Emma', 'jessa', 'Kelly']
+
+school = 'ABC School'
+name=['tt','yy','rr','hh']
+
+
+# calculate count
+print(len(students))
+print(len(school))
+print(len(name))
